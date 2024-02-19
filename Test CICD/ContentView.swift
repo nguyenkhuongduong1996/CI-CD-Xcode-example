53ebe6af431899d253c9852a80803f76f673bfc6
Duong-Maxmobile <105182526+Duong-Maxmobile@users.noreply.github.com>
2024-02-19 14:49:47 +0700

    [Update] - test workflow

--- Test CICD/ContentView.swift
+++ Test CICD/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello world, test work flow!")
+            Text("Hello world, test work flow 2!")
         }
         .padding()
     }
