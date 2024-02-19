1a90a75f0881fa9d205c35b8271b6412ee355502
Duong-Maxmobile <105182526+Duong-Maxmobile@users.noreply.github.com>
2024-02-19 13:37:45 +0700

    [Update] - appicon

--- Test CICD/ContentView.swift
+++ Test CICD/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello my world!")
+            Text("Hello my world haha nnnnn!")
         }
         .padding()
     }
