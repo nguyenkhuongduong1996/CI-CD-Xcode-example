7438d9b0deda108550de110f0542eb992020d4c1
Duong-Maxmobile <105182526+Duong-Maxmobile@users.noreply.github.com>
2024-02-19 09:41:00 +0700

    Initial Commit

---
+++ Test CICD/Test_CICDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_CICDApp.swift
+//  Test CICD
+//
+//  Created by duongnk@maxmobile-software.com on 19/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Test_CICDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
